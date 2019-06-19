195b3c33c429239df97ba51946b549a06a051d05
Jack Park <jackpark@gmail.com>
2019-06-19 16:15:34 -0700

    first commit

---
+++ src/main/java/org/topicquests/ks/utils/PersistentMap.java
@@ -0,0 +1,133 @@
+/*
+ * Copyright 2019, TopicQuests
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.topicquests.ks.utils;
+
+import java.io.File;
+
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
+import org.mapdb.serializer.SerializerCompressionWrapper;
+
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
+
+/**
+ * @author jackpark
+ *
+ */
+public class PersistentMap {
+	private DB database;
+	private HTreeMap<String, String> map;
+	private boolean isClosed = true;
+
+	/**
+	 * @param databasePath e.g. data/mydata
+	 * @param storeName e.g. mystore
+	 */
+	public PersistentMap(String databasePath, String storeName) {
+		try {
+			File f = new File(databasePath);
+			System.out.println("PS-0a "+f);
+			database = DBMaker.fileDB(f)
+					.closeOnJvmShutdown()
+					//.transactionDisable()  defaults to that
+					//.compressionEnable()
+					.make();
+			map = database.hashMap(storeName)
+	        	.keySerializer(Serializer.STRING)
+	        	.valueSerializer(new SerializerCompressionWrapper(Serializer.STRING))
+	        	.createOrOpen();
+			isClosed = false;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	/**
+	 * Returns <code>true</code> if this overwrites a previous value
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public boolean put(String key, String value) {
+		Object o = map.put(key, value);
+		return o != null;
+	}
+	
+	/**
+	 * Can return <code>null</code>
+	 * @param key
+	 * @return
+	 */
+	public Object get(String key) {
+		return map.get(key);
+	}
+	
+	public boolean containsKey(String key) {
+		return map.get(key) != null;
+	}
+	
+	public int size() {
+		return map.size();
+	}
+	
+	/**
+	 * Utility function for dealing with JSON
+	 * @param key
+	 * @param value
+	 * @return
+	 */
+	public boolean putJSON(String key, JSONObject value) {
+		return put(key, value.toJSONString());
+	}
+	
+	/**
+	 * Utility function for dealing with JSON
+	 * @param key
+	 * @return
+	 * @throws Exception
+	 */
+	public JSONObject getJSON(String key) throws Exception {
+		String json = (String)map.get(key);
+		if (json == null)
+			return null;
+		JSONParser p = new JSONParser(JSONParser.MODE_JSON_SIMPLE);
+		return (JSONObject)p.parse(json);
+	}
+	
+	public Object remove(String key) {
+		return map.remove(key);
+	}
+	
+	/**
+	 * Important to close this database
+	 */
+	public void shutDown() {
+		if (!isClosed) {
+			System.out.println("PersistentMap shutting down");
+			database.commit();
+			//database.compact();
+			database.close();
+			System.out.println("PersistentMap closed");
+			isClosed = true;
+		}
+	}
+	
+
+}
---
+++ src/main/java/org/topicquests/ks/utils/PersistentSet.java
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2019, TopicQuests
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions
+ * and limitations under the License.
+ */
+package org.topicquests.ks.utils;
+
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.NavigableSet;
+
+/**
+ * @author jackpark
+ * 
+ */
+public class PersistentSet {
+	private DB database;
+	private NavigableSet<String> treeSet;
+	private boolean isClosed = true;
+	
+	/**
+	 * 
+	 * @param databasePath
+	 * @param storeName
+	 */
+	public PersistentSet(String databasePath, String storeName) {
+		try {
+			File f = new File(databasePath);
+			System.out.println("PS-0a "+f);
+			database = DBMaker.fileDB(f)
+					.closeOnJvmShutdown()
+					//.transactionDisable()  defaults to that
+					//.compressionEnable()
+					.make();
+			System.out.println("PS-1 "+database);
+			treeSet = (NavigableSet<String>)database.treeSet(storeName).createOrOpen();
+			System.out.println("PS-2 "+treeSet.size());
+			isClosed = false;
+		} catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
+	
+	/**
+	 * Returns <code>true</code> if <code>val</code> is added
+	 * @param val
+	 * @return
+	 */
+	public boolean add(String val) {
+		return treeSet.add(val);
+	}
+	
+	public boolean contains(String val) {
+		return treeSet.contains(val);
+	}
+	
+	/**
+	 * Iterator on the values
+	 * @return
+	 */
+	public Iterator<String> iterator() {
+		return treeSet.iterator();
+	}
+	
+	public int size() {
+		return treeSet.size();
+	}
+	
+	/**
+	 * Returns <code>true</code> if <code>val</code> is removed
+	 * @param val
+	 * @return
+	 */
+	public boolean remove(String val) {
+		return treeSet.remove(val);
+	}
+	
+	/**
+	 * Important to close the database
+	 */
+	public void shutDown() {
+		if (!isClosed) {
+			System.out.println("PersistentSet shutting down");
+			database.commit();
+			//database.compact();
+			database.close();
+			System.out.println("PersistentSet closed");
+			isClosed = true;
+		}
+	}
+
+
+}
